0a78b116639e6caa1a4783795e1a89b66f118a83
Darth Varg <cbvargas123@gmail.com>
2020-10-25 23:59:46 -0700

    created base code for contact models

---
+++ server/database/models/contacts.js
@@ -0,0 +1,11 @@
+const pool = require('../index')
+const {
+    GET_ALL_CONTACTS,
+    CREATE_CONTACT,
+    UPDATE_CONTACT,
+    DELETE_CONTACT
+} = require('./param-queries').contactTableQueryStrings
+
+module.exports = {
+    getAllContactsFromDatabase = () => pool.query(GET_ALL_CONTACTS)
+}
