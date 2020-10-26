9e35738f0fd05f15b0ed32b4d0f1e36f07eca713
Darth Varg <cbvargas123@gmail.com>
2020-10-25 21:10:38 -0700

    added route to endpoint

--- server/database/index.js
+++ server/database/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT || 3131;
+const port = process.env.PORT || 3232;
 const path = require('path');
+const CONTACTS = require('./routers/contacts');
+const CONTACT_DETAILS = require('./routers/details');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
 //routes
+app.use('/api/contacts', CONTACTS);
+app.use('/api/details', CONTACT_DETAILS);
 
 app.listen(port, err => err ? console.error('SERVER ERROR: ', err) : console.log(`Server is listening on port ${port}`))
