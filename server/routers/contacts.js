ce446df23c2363d7aab700b242ac2824addbea3d
Darth Varg <cbvargas123@gmail.com>
2020-10-26 00:07:56 -0700

    created base code for contact routers

---
+++ server/routers/contacts.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const contactControllers = require('../controllers/contacts')
+
+router
+    .route('/')
+    .get(contactControllers.getAllContacts)
+
+
+module.exports = router
