982b292b7c5674cd950bf8ff4b3d030c2fc35a6e
Darth Varg <cbvargas123@gmail.com>
2020-10-25 23:50:52 -0700

    created queries for contact models

---
+++ server/database/models/param-queries.js
@@ -0,0 +1,26 @@
+module.exports = {
+    contactTableQueryStrings = {
+        GET_ALL_CONTACTS: "SELECT * FROM contacts",
+        CREATE_CONTACT: `
+          INSERT INTO contacts
+          (first_name, last_name, email, category, notes)
+          VALUES
+          ($1, $2, $3, $4, $5)
+        `,
+        UPDATE_CONTACT: `
+          UPDATE contacts
+          SET
+          first_name = $2,
+          last_name = $3,
+          email = $4,
+          category = $5,
+          notes = $6
+          WHERE contact_id = $1
+        `,
+        DELETE_CONTACT: `
+          DELETE FROM contacts
+          WHERE contact_id = $1
+        `
+    }
+
+}
