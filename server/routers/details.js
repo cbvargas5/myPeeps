ecb6afdf1a0c344ece44f31990d2e46b45a11a0c
Darth Varg <cbvargas123@gmail.com>
2020-10-26 23:46:47 -0700

    created base code for detail routes

---
+++ server/routers/details.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+    // const detailControllers = require('../controllers/details')
+
+// router
+//     .route('/:id')
+//     .get(detailControllers.getAllDetails)
+
+// router
+//     .route('/phone/:id')
+//     .post(detailControllers.createPhoneNumber)
+//     .put(detailControllers.updatePhoneNumber)
+//     .delete(detailControllers.deletePhoneNumber)
+
+// router
+//     .route('/address/:id')
+//     .post(detailControllers.createAddress)
+//     .put(detailControllers.updateAddress)
+//     .delete(detailControllers.deleteAddress)
+
+
+module.exports = router
