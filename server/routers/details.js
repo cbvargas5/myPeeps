9f58a3f5bf99a99784d69a4df8871c83a4827a75
Darth Varg <cbvargas123@gmail.com>
2020-10-27 15:50:53 -0700

    completed routes for phone and address

--- server/routers/details.js
+++ server/routers/details.js
@@ -2,21 +2,24 @@ const express = require('express')
 const router = express.Router()
 const detailControllers = require('../controllers/details')
 
+//contact id
 router
     .route('/:id')
     .get(detailControllers.getAllDetailsForContact)
 
-// router
-//     .route('/phone/:id')
-//     .post(detailControllers.createPhoneNumber)
-//     .put(detailControllers.updatePhoneNumber)
-//     .delete(detailControllers.deletePhoneNumber)
+//contact id for post request ~~~ phone id for put and delete request
+router
+    .route('/phone/:id')
+    .post(detailControllers.createPhoneNumber)
+    .put(detailControllers.updatePhoneNumber)
+    .delete(detailControllers.deletePhoneNumber)
 
-// router
-//     .route('/address/:id')
-//     .post(detailControllers.createAddress)
-//     .put(detailControllers.updateAddress)
-//     .delete(detailControllers.deleteAddress)
+//contact id for post request ~~~ address id for put and delete request
+router
+    .route('/address/:id')
+    .post(detailControllers.createAddress)
+    .put(detailControllers.updateAddress)
+    .delete(detailControllers.deleteAddress)
 
 
 module.exports = router
