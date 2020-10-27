96a6e0dffda41a35ad25ad46b22543acafaebd05
Darth Varg <cbvargas123@gmail.com>
2020-10-27 15:18:48 -0700

    completed models for updating address/phone numbers

--- server/database/models/details.js
+++ server/database/models/details.js
@@ -15,5 +15,13 @@ const {
 
 module.exports = {
     getAllAddressesForContact: id => pool.query(GET_CONTACT_ADDRESSES, [id]),
-    getAllPhoneNumbersForContact: id => pool.query(GET_CONTACT_PHONE_NUMBERS, [id])
+    getAllPhoneNumbersForContact: id => pool.query(GET_CONTACT_PHONE_NUMBERS, [id]),
+
+    createAddressRecord: (...addressColumnsToAdd) => pool.query(CREATE_ADDRESS, addressColumnsToAdd),
+    updateAddressRecord: (addressId, ...addressColumnsToUpdate) => pool.query(UPDATE_ADDRESS, [addressId, ...addressColumnsToUpdate]),
+    deleteAddressRecord: id => pool.query(DELETE_ADDRESS, [id]),
+
+    createPhoneNumRecord: (...phoneNumberColumnsToAdd) => pool.query(CREATE_PHONE_NUMBER, phoneNumberColumnsToAdd),
+    updatePhoneNumRecord: (phoneNumberId, ...phoneNumberColumnsToUpdate) => pool.query(UPDATE_PHONE_NUMBER, [phoneNumberId, ...phoneNumberColumnsToUpdate]),
+    deletePhoneNumRecord: id => pool.query(DELETE_PHONE_NUMBER, [id])
 }
