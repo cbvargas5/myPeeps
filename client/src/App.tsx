fa81dc3e2ed72c71e107677ce50760e290560fee
Darth Varg <cbvargas123@gmail.com>
2020-10-30 14:10:19 -0700

    imported components and added react router routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,22 @@
 import React from "react"
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import ContactPage from './components/pages/ContactPage'
+import DetailsPage from './components/pages/DetailsPage'
+import EditContactPage from './components/pages/EditContactPage'
 
 const App: React.FC = () => {
   return (
-    <h1>My React and TypeScript App</h1>
+    <>
+      <BrowserRouter>
+        <main>
+          <Switch>
+            <Route path="/" exact component={ContactPage}/>
+            <Route path="/contact-details" exact component={DetailsPage}/>
+            <Route path="/edit-contact" exact component={EditContactPage}/>
+          </Switch>
+        </main>
+      </BrowserRouter>
+    </>
   );
 }
 
