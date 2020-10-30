0250ba8015d74ef09fcc22f513b6eda3fb256b5e
Darth Varg <cbvargas123@gmail.com>
2020-10-30 14:19:26 -0700

    created jsx structure for contact page

--- client/src/components/pages/ContactPage.tsx
+++ client/src/components/pages/ContactPage.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import Contact from '../Contact';
 
 interface ContactPageProps {
 
 }
 
-const ContactPage: React.FC<ContactPageProps> = ({}) => {
-    return (
-      <h1>Hello from ContactPage</h1>
-    );
+const ContactPage: React.FC<ContactPageProps> = ({ }) => {
+  return (
+    <section className="contact-page">
+      <header>
+        <h1>myPEEPS</h1>
+      </header>
+      <section className="big-card">
+        {/* search */}
+        {/* add contact button */}
+        {/* list of contacts */}
+      </section>
+    </section>
+  );
 }
 
 export default ContactPage
