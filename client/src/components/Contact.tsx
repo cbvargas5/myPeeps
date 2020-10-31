4a48ac83189253ce89e082dc9151fa75b2e57745
Darth Varg <cbvargas123@gmail.com>
2020-10-31 15:08:31 -0700

    created contact component

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
 interface ContactProps {
-
+  contactId: string;
+  firstName: string;
+  lastName: string | null;
+  category: string | null;
 }
 
-const Contact: React.FC<ContactProps> = ({}) => {
-    return ();
+const Contact: React.FC<ContactProps> = ({ contactId, firstName, lastName, category }) => {
+  return (
+    <div data-contact-id={contactId} className="contact-wrapper">
+      <div className="contact-name">{firstName} {lastName && lastName}</div>
+      <div className="category-title">{category && category}</div>
+    </div>
+  );
 }
 
 export default Contact
