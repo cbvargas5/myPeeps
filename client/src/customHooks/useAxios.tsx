7086e7fb292ddb212db1169364bc9172a95bebac
Darth Varg <cbvargas123@gmail.com>
2020-10-30 22:18:01 -0700

    created expected interfaces

--- client/src/customHooks/useAxios.tsx
+++
@@ -1,8 +0,0 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-
-export const useAxios = (url: string, method: string | undefined = 'get') => {
-  useEffect(() => {
-// do stuff
-  }, [url, method])
-}
---
+++ client/src/customHooks/useGet.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
+interface Contact {
+  contact_id: string | null;
+  first_name: string | null;
+  last_name: string | null;
+  category: string | null;
+  notes: string | null;
+}
+
+interface Address {
+  address_id: string;
+  contact_id: string;
+  street_address: string | null;
+  city: string | null;
+  state_name: string | null;
+  zip: string | null;
+}
+
+interface PhoneNumber {
+  phone_id: string;
+  contact_id: string;
+  phone_num: string;
+}
+
+interface ContactDetails {
+  addresses: Address[];
+  phoneNumbers: PhoneNumber[];
+}
+
+interface getMethodResponseState {
+  responseData: Contact[] | ContactDetails | null;
+  isLoading: boolean;
+}
+
+export const useGet = (url: string) => {
+
+  const [getMethodResponse, setGetMethodResponse] = useState<getMethodResponseState>()
+
+  useEffect(() => {
+    // do stuff
+  }, [url])
+}
