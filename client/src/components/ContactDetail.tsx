9f18db50d8331d829bdb27acd674d4fc65751f4b
Darth Varg <cbvargas123@gmail.com>
2020-10-30 21:53:05 -0700

    base git st

--- client/src/components/ContactDetail.tsx
+++ client/src/components/ContactDetail.tsx
@@ -1,11 +1,36 @@
 import React from 'react'
 
-interface ContactDetailProps {
-
+interface Address {
+  addressId: string;
+  streetAddress: string;
+  city: string;
+  stateName: string;
+  zip: string;
 }
 
-const ContactDetail: React.FC<ContactDetailProps> = ({}) => {
-    return ();
+interface ContactDetailProps {
+  contactId: string;
+  detailTitle: string;
+  detailInfo: string | Address;
+}
+
+const ContactDetail: React.FC<ContactDetailProps> = ({ contactId, detailTitle, detailInfo }) => {
+
+  return (
+    <div data-contact-id={contactId} className="contact-detail-wrapper">
+      <div className="detail-title">{detailTitle}</div>
+      {
+        typeof detailInfo == "string"
+          ?
+          <div className="detail-info-line-1">{detailInfo}</div>
+          :
+          <div data-address-id={detailInfo.addressId} className="address-block">
+            <div className="detail-info-line-1">{detailInfo.streetAddress}</div>
+            <div className="detail-info-line-2">{`${detailInfo.city} ${detailInfo.stateName}, ${detailInfo.zip}`}</div>
+          </div>
+      }
+    </div>
+  );
 }
 
 export default ContactDetail
