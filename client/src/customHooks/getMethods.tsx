c5ab2b0dabe3b828b1d85de01170fd023bf854f8
Darth Varg <cbvargas123@gmail.com>
2020-11-19 23:54:24 -0800

    brought back getMethod for refactor

--- client/src/customHooks/getMethods.tsx
+++ client/src/customHooks/getMethods.tsx
@@ -2,21 +2,19 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { ContactData, ContactListGetMethodResponseState } from '../types'
 
-// commented for refactor
 
+export const useGetContacts = () => {
+  const ENDPOINT: string = '/api/contacts'
+  console.log(ENDPOINT)
+  const [getMethodResponse, setGetMethodResponse] = useState<ContactListGetMethodResponseState>({ responseData: null, isLoading: true })
 
-// export const useGetContacts = () => {
-//   const ENDPOINT: string = '/api/contacts'
-//   console.log(ENDPOINT)
-//   const [getMethodResponse, setGetMethodResponse] = useState<ContactListGetMethodResponseState>({ responseData: null, isLoading: true })
+  useEffect(() => {
+    setGetMethodResponse({ responseData: null, isLoading: true })
+    axios.get<ContactData[]>(ENDPOINT)
+      .then(({ data }) => {
+        setGetMethodResponse({ responseData: data, isLoading: false })
+      })
+  }, [])
 
-//   useEffect(() => {
-//     setGetMethodResponse({ responseData: null, isLoading: true })
-//     axios.get<ContactData[]>(ENDPOINT)
-//       .then(({ data }) => {
-//         setGetMethodResponse({ responseData: data, isLoading: false })
-//       })
-//   }, [])
-
-//   return getMethodResponse
-// }
+  return getMethodResponse
+}
